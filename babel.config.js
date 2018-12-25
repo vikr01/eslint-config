7f42d42a6df8bdf1de96660903a14a523a5e5ea8
vikr01 <vik120@icloud.com>
2018-12-25 05:16:53 -0800

    add babel plugins needed for testing coverage

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const testPlugins = [
+  require('@babel/plugin-syntax-dynamic-import'),
+  require('@babel/plugin-syntax-jsx'),
+  require('@babel/plugin-proposal-class-properties'),
+  [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+];
 const { workspaces = [] } = require('./package.json');
 
 module.exports = api => {
   const isProdEnv = api.env('production');
+  const isTestEnv = api.env('test');
 
   const targets = {};
   if (isProdEnv) {
@@ -22,5 +29,6 @@ module.exports = api => {
       ],
       require('@babel/preset-flow'),
     ],
+    plugins: [...(!isTestEnv ? [] : testPlugins)],
   };
 };
