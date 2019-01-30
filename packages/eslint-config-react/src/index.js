2a6b4bbdb88aeb102d6156b3bb1b6c0a8ba82630
vikr01 <vik120@icloud.com>
2019-01-29 20:59:25 -0800

    add peerdep-fixer

--- packages/eslint-config-react/src/index.js
+++ packages/eslint-config-react/src/index.js
@@ -1,1 +1,8 @@
-export default {};
+export default {
+  extends: [
+    require.resolve('eslint-config-airbnb'),
+    require.resolve('@vikr01/eslint-config'),
+  ],
+  plugins: ['react', 'jsx-a11y'],
+  rules: {},
+};
