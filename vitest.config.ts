d6ee48347dae9919a0fc0028074f729389cb36a5
vikr01 <vik120@icloud.com>
2025-05-03 01:41:04 -0700

    get tests working by moving to jest

---
+++ vitest.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vitest/config';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+export default defineConfig({
+  test: {
+    deps: {
+      moduleDirectories: ['node_modules'],
+    },
+    exclude: ['**/node_modules/**', 'packages/*/dist/**'],
+    include: ['**/*.test.{ts,tsx}'],
+    globals: true,
+
+    reporters: ['verbose'],
+    typecheck: {
+      tsconfig: require.resolve('./tsconfig.json'),
+    },
+
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage/',
+      reporter: ['text', 'lcov'],
+
+      include: ['packages/*/src/**/*.{ts,tsx}'],
+
+      exclude: [
+        '**/.*',
+        '**/.*/**',
+        '**/*.config.{js,ts}',
+        'packages/*/dist/**',
+        'scripts/**',
+        'coverage/**',
+      ],
+    },
+
+    restoreMocks: true,
+    clearMocks: true,
+    mockReset: true,
+    watch: false,
+  },
+});
